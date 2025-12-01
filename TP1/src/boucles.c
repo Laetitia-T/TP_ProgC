9592a4c83a741af6e67ef6fc8e0d18aaa7bcb71d
Laetitia-T <151835304+Laetitia-T@users.noreply.github.com>
2025-12-01 13:57:39 +0100

    Exercice fini

---
+++ TP1/src/boucles.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int compteur = 5;  
+
+    if (compteur > 10) {
+        return 1;
+    }
+
+    for (int i = 1; i <= compteur; i++) {
+
+        for (int j = 1; j <= i; j++) {
+
+            if (i == 1 || i == compteur) {
+                printf("* ");   
+            } else if (j == 1 || j == i) {
+                printf("* ");   
+            } else {
+                printf("# ");
+            }
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
