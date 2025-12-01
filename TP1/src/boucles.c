a71b3a7262a54e5c936d3b204b91d81e1a423b2c
Laetitia-T <151835304+Laetitia-T@users.noreply.github.com>
2025-12-01 15:22:05 +0100

    Exercice fini
    Added a new implementation using a while loop to print a pattern.

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -26,3 +26,36 @@ int main() {
 
     return 0;
 }
+
+////
+
+#include <stdio.h>
+
+int main(void) {
+    int compteur = 5;
+    
+    if (compteur >= 10) {
+        return 1;
+    }
+
+    int i = 1, j = 1;
+    while (i <= compteur) {
+        if (j > i) {
+            printf("\n");
+            i++;
+            j = 1;
+        } else {
+            if (i == 1 || i == compteur) {
+                printf("*");
+            } else if (j == 1 || j == i) {
+                printf("*");
+            } else {
+                printf("#");
+            }
+            if (j < i) printf(" ");
+            j++;
+        }
+    }
+
+    return 0;
+}
